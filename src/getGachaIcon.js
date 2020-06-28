de17d3b372dfda22fc4410940704563ad870e7e5
RedBeanN <me@hongshn.xyz>
2020-06-28 11:25:31 +0800

    get gacha icon

---
+++ src/getGachaIcon.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+
+let icons = null;
+
+const getGachaIcon = (conf = {
+  type: 'svt',
+  id: 1,
+}) => {
+  if (!icons) icons = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../statics/icons.json')));
+  const { type, id } = conf;
+  let img = '';
+  if (type === 'svt') img = icons.svtIcons[id - 1];
+  else img = icons.cftIcons[id - 1];
+  return path.resolve(__dirname, '../statics/images/icons', img.replace('/images', '').replace(/\//g, '_'));
+};
+
+module.exports = getGachaIcon;
