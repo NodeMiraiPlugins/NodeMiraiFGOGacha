37ea3292cf81ed1f5bd3eb041c9f05e764b338ad
RedBeanN <me@hongshn.xyz>
2020-06-28 11:08:53 +0800

    init index

--- index.js
+++ index.js
@@ -2,4 +2,24 @@ const fs = require('fs');
 const path = require('path');
 const init = require('./init');
 
-fs.existsSync(path.resolve(__dirname, '.init')) ? init() : init(true, true);
+if (!fs.existsSync(path.resolve(__dirname, '.init'))) {
+  init(true, true);
+}
+
+/**
+ * @method FGOGacha
+ * @description NodeMiraiSDK FGO Gacha plugin
+ * @param { object } config
+ */
+const FGOGacha = ({
+  cooldown = 60000,
+}) => {
+  const gachaCooldown = [];
+};
+
+// 普通的更新一下
+FGOGacha.update = () => init();
+// 强制初始化
+FGOGacha.init = () => init(true, true);
+
+module.exports = FGOGacha;
