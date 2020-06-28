33b79f0f5f6705fdb99831c7978d957ecad5e929
RedBeanN <me@hongshn.xyz>
2020-06-28 10:13:33 +0800

    add init.js

---
+++ init.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const download = require('./src/download');
+
+const mooncellBackgroundUrl = 'https://fgo.wiki/images/bg/bg-mc-icon.png';
+const mooncellBackgroundPath = path.resolve(__dirname, './statics/bg-mc-icon.png');
+
+/**
+ * @method init
+ * @description init resources
+ * @param { boolean } [log] show log
+ * @param { boolean } [overwrite] force download and overwrite files
+ */
+const init = async (log = false, overwrite = false) => {
+  const info = (...t) => log && console.log(...t);
+  await download(mooncellBackgroundUrl, mooncellBackgroundPath, overwrite);
+  info(`[Init] bg-mc-icon.png`);
+};
+
+module.exports = init;
