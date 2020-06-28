676130afedd21ba45ba2b2fdd79280a5d2a04476
RedBeanN <me@hongshn.xyz>
2020-06-28 10:30:43 +0800

    update init

--- init.js
+++ init.js
@@ -1,5 +1,9 @@
+const fs = require('fs');
 const path = require('path');
 const download = require('./src/download');
+const getGachaPools = require('./src/getGachaPools');
+
+const baseDir = path.resolve(__dirname, './statics');
 
 const mooncellBackgroundUrl = 'https://fgo.wiki/images/bg/bg-mc-icon.png';
 const mooncellBackgroundPath = path.resolve(__dirname, './statics/bg-mc-icon.png');
@@ -12,8 +16,15 @@ const mooncellBackgroundPath = path.resolve(__dirname, './statics/bg-mc-icon.png
  */
 const init = async (log = false, overwrite = false) => {
   const info = (...t) => log && console.log(`[init]`, ...t);
+  if (!fs.existsSync(baseDir)) {
+    fs.mkdirSync(baseDir);
+    fs.mkdirSync(path.resolve(baseDir, 'icons'));
+    fs.mkdirSync(path.resolve(baseDir, 'gacha'));
+    info('Created statics/ statics/icons/ statics/gacha/');
+  }
   await download(mooncellBackgroundUrl, mooncellBackgroundPath, overwrite);
   info(`Downloaded bg-mc-icon.png`);
+  await getGachaPools(info);
 };
 
 module.exports = init;
