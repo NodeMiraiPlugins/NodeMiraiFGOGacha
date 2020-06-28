e2c3e90258aa2082681a5de1973fd9408ddc1ea2
RedBeanN <me@hongshn.xyz>
2020-06-28 11:17:28 +0800

    update index

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const fs = require('fs');
 const path = require('path');
+
+const Mirai = require('node-mirai-sdk');
+const { Image } = Mirai.MessageComponent;
+
 const init = require('./init');
 
 if (!fs.existsSync(path.resolve(__dirname, '.init'))) {
@@ -19,8 +23,22 @@ const saveDb = () => {
  */
 const FGOGacha = ({
   cooldown = 60000,
+  allowGroup = true,
+  allowPrivate = false,
 }) => {
-  const gachaCooldown = [];
+  const gachaCooldown = {
+    group: [],
+    sender: [],
+  };
+  const callback = async ({
+    message,
+    bot,
+  }) => {
+    const { sender, messageChain, reply } = message;
+    const { group } = sender;
+    if (!group && !allowPrivate) return;
+    if (group && !allowGroup) return;
+  };
 };
 
 // 普通的更新一下
